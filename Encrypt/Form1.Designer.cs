5843487ded4a038f17caa4d8fcbe3b26f3ee1cb8
Daryl Krans <wadzinsky@hotmail.com>
2022-01-11 22:55:23 -0600

    Fixed Password and extension method

--- Encrypt/Form1.Designer.cs
+++ Encrypt/Form1.Designer.cs
@@ -31,11 +31,12 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -54,21 +55,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Input File";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.DimGray;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.LightGray;
-            this.button2.Location = new System.Drawing.Point(94, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Out File";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button3
             // 
@@ -76,20 +63,20 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.LightGray;
-            this.button3.Location = new System.Drawing.Point(258, 13);
+            this.button3.Location = new System.Drawing.Point(94, 13);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Start";
             this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.ForeColor = System.Drawing.Color.LightGray;
-            this.label1.Location = new System.Drawing.Point(14, 70);
+            this.label1.Location = new System.Drawing.Point(14, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(78, 13);
             this.label1.TabIndex = 3;
@@ -97,34 +84,60 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(98, 68);
+            this.textBox1.Location = new System.Drawing.Point(98, 42);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(235, 20);
+            this.textBox1.Size = new System.Drawing.Size(278, 20);
             this.textBox1.TabIndex = 4;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.ForeColor = System.Drawing.Color.LightGray;
-            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Location = new System.Drawing.Point(10, 69);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(34, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "         ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.LightGray;
+            this.label3.Location = new System.Drawing.Point(10, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "         ";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.ForeColor = System.Drawing.Color.LightGray;
+            this.checkBox1.Location = new System.Drawing.Point(207, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox1.Size = new System.Drawing.Size(169, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Delete Source  file when done";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
-            this.ClientSize = new System.Drawing.Size(345, 97);
+            this.ClientSize = new System.Drawing.Size(388, 107);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
@@ -140,10 +153,11 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
--- Encrypt/Form1.cs
+++ Encrypt/Form1.cs
@@ -12,142 +12,136 @@ namespace Encrypt
         string OutFile = "";
         string Key = " ";
         int dec = 0;
-
-        void clearstrings()
+        bool DelFile;
+        private byte[] XOR(byte[] DataIn, string EncKey)
+        {
+            byte[] DataOut = new byte[DataIn.Length];
+            for (int t = 0; t < DataIn.Length; t++)
+            {
+                DataOut[t] = (byte)(DataIn[t] ^ EncKey[t % EncKey.Length] ^ ((DataIn.Length - t) * 124));
+            }
+            return DataOut;
+        }
+        void Clearstrings()
         {
             InFile = "";
             OutFile = "";
             Key = " ";
             textBox1.Text = "";
             label2.Text = "";
+            label3.Text = "";
             dec = 0;
+            checkBox1.Checked = false;
+            DelFile = checkBox1.Checked;
+            Truncate(Key, 1);
+        }
+        private string Truncate(string value, int maxChars)
+        {
+            byte[] trunc = Encoding.ASCII.GetBytes(value);
+            byte[] dat = new byte[maxChars];
+            byte[] o = Encoding.ASCII.GetBytes("...");
+            string newvalue;
+            if (value.Length > maxChars)
+            {
+                for (int i = 0; i < 3; i++) dat[i] = o[i];
+                int t = 3;
+                {
+                    for (int i = value.Length - (maxChars) + 3; t < (maxChars); i++)
+                    {
+                        dat[t] = trunc[i];
+                        t++;
+                    }
+                }
+                newvalue = Encoding.ASCII.GetString(dat);
+            }
+            else newvalue = value;
+            return newvalue;
         }
         private string GetExtension(long size)
         {
-            string hi = "";
-            byte[] buff = new byte[8];
+            byte[] buff = new byte[108];
             FileStream Source = new(InFile, FileMode.Open, FileAccess.Read);   // Open source file for read only
-            Source.Seek(size - 8, SeekOrigin.Begin);
-            Source.Read(buff, 0, 8);
+            Source.Seek(size - 108, SeekOrigin.Begin);
+            Source.Read(buff, 0, 108);
             Source.Close();
-            string conv = Encoding.Default.GetString(buff);
-            StringBuilder In = new StringBuilder(conv);
-            StringBuilder Out = new StringBuilder(4); //(conv.Length);
-            StringBuilder Out2 = new StringBuilder(4); //(conv.Length);
-            char cha;
-            for (int i = 0; i < conv.Length; i++)
-            {
-                cha = In[i];
-                cha = (char)(cha ^ Key[i % Key.Length]);
-                if (i < 4) Out.Append(cha);
-                if (i >= 4) Out2.Append(cha);
-            }
-            if (Out2.ToString() == "true") hi = Out.ToString();
-            else hi = "You FAILED miserably!!";
-            return hi;
+            string test = Encoding.Default.GetString(XOR(buff, Key));
+            if (test != "This is a test to see if the password entered is correct. If it is correct, this will be readable in english") test = "denied!";
+            return test;
         }
-        void checkstatus()
+        void Checkstatus()
         {
             try
             {
-                if ((Key.Length == 0) || (InFile.Length == 0) || (OutFile.Length == 0)) button3.Enabled = false;
+                if ((Key == " ") || (InFile.Length == 0) || (OutFile.Length == 0)) button3.Enabled = false;
                 else button3.Enabled = true;
-                if (InFile.Length == 0) button2.Enabled = false;
-                else button2.Enabled = true;
-                if (dec == 1) button2.Enabled = false;
+                if (InFile.Length == 0) checkBox1.Enabled = false;
+                else checkBox1.Enabled = true;
             }
             catch { }
         }
         public Form1()
         {
             InitializeComponent();
-            checkstatus();
+            Checkstatus();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
             openFileDialog1.Title = "Select input file";
             openFileDialog1.FileName = "";
             openFileDialog1.ShowDialog();
             InFile = openFileDialog1.FileName;
             string ext = Path.GetExtension(InFile);
-            if (ext == ".enc") dec = 1;
-            if (dec == 1)
+            if (ext == ".enc")
             {
-                long size = new System.IO.FileInfo(InFile).Length;
-                ext = GetExtension(size);
-                OutFile = Path.ChangeExtension(InFile, ext);
+                dec = 1;
+                OutFile = Path.GetDirectoryName(InFile) + @"\" + Path.GetFileNameWithoutExtension(InFile);
             }
-            checkstatus();
-            label2.Text = "Input File : " + InFile;
+            else OutFile = InFile + ".enc";
+            Checkstatus();
+            UpdateLabels();
         }
-
-        private void button2_Click(object sender, EventArgs e)
+        void UpdateLabels()
         {
-            string ext = ".enc";
-
-            saveFileDialog1.Title = "Select output file";
-            saveFileDialog1.FileName = Path.GetFileNameWithoutExtension(InFile) + ext;
-            saveFileDialog1.ShowDialog();
-            OutFile = (saveFileDialog1.FileName);
-            checkstatus();
-            label2.Text = "Output File : " + OutFile;
+            label2.Text = "Input File : " + Truncate(InFile, 55);
+            label3.Text = "Output File: " + Truncate(OutFile, 55);
         }
-
-        private void button3_Click(object sender, EventArgs e)
+        private void Button3_Click(object sender, EventArgs e)
         {
-            int fail = 0;
-            string ext = Path.GetExtension(InFile) + "true";
+            // Get file Length and set variables to handle file file in 1mb chunks
+            string ext = "This is a test to see if the password entered is correct. If it is correct, this will be readable in english";
             long size = new System.IO.FileInfo(InFile).Length;
             long chunk = 1024 * 1024;
             long sections = size / chunk;
-            long rest = size - (sections * chunk);
             long chunksize = chunk;
-            long p;
             string encdec = "Encrypting File ";
+            long p;
+            int fail = 0;
             if (dec == 1)
             {
                 ext = GetExtension(size);
-                if (ext != "You FAILED miserably!!")
+                if (ext != "denied!")
                 {
-                    OutFile = Path.ChangeExtension(InFile, ext);
-                    size = size - 8;
+                    size -= 108;
                     sections = size / chunk;
-                    rest = size - (sections * chunk);
                     encdec = "Decrypting File ";
                 }
                 else fail = 1;
             }
             if (fail == 0)
             {
-                Thread crypt = new Thread(() =>
+                byte[] TestPhrase = XOR(Encoding.ASCII.GetBytes(ext), Key);
+                FileStream Source = new(InFile, FileMode.Open, FileAccess.Read);   // Open source file for read only
+                FileStream Dest = new(OutFile, FileMode.Create, FileAccess.Write);  // Open Destination file for append
+                // Start new Thread  -------------------------------- //
+                Thread crypt = new(() =>
                 {
-                    StringBuilder In = new StringBuilder(ext);
-                    StringBuilder Out = new StringBuilder(ext.Length);
-                    char cha;
-                    for (int i = 0; i < ext.Length; i++)
-                    {
-                        cha = In[i];
-                        cha = (char)(cha ^ Key[i % Key.Length]);
-                        Out.Append(cha);
-                    }
-                    byte[] extt = Encoding.ASCII.GetBytes(Out.ToString());
-                    this.Invoke(new Action(() => label2.Text = "Creating " + OutFile));
-                    FileStream Source = new(InFile, FileMode.Open, FileAccess.Read);   // Open source file for read only
-                    FileStream Dest = new(OutFile, FileMode.Create, FileAccess.Write);  // Open Destination file for append
-                                                                                        //Dest.Write(extt, 0, extt.Length);
                     for (long i = 0; i <= sections; i++)
                     {
-                        if (i == sections) chunksize = rest;
+                        if (i == sections) chunksize = size - (sections * chunk);
                         byte[] buff = new byte[chunksize];
                         Source.Seek(i * chunk, SeekOrigin.Begin);           // seeks file location in source file
                         Source.Read(buff, 0, (int)chunksize);
-                        byte[] output = new byte[buff.Length];
-                        for (int t = 0; t < buff.Length; t++)
-                        {
-                            output[t] = (byte)(buff[t] ^ Key[t % Key.Length] ^ ((buff.Length - t) * 124));
-                        }
-
+                        byte[] output = XOR(buff, Key);
                         Dest.Write(output, 0, output.Length);
                         if (sections > 0)
                         {
@@ -155,21 +149,35 @@ namespace Encrypt
                             this.Invoke(new Action(() => this.Text = encdec + p.ToString() + "%"));
                         }
                     }
-                    if (dec == 0) Dest.Write(extt, 0, extt.Length);
+                    if (dec == 0) Dest.Write(TestPhrase, 0, TestPhrase.Length);
                     Source.Close();
                     Dest.Close();
+                    if (DelFile == true) File.Delete(InFile);
                     this.Invoke(new Action(() => this.Text = "Encrypt"));
-                    this.Invoke(new Action(() => clearstrings()));
+                    this.Invoke(new Action(() => Clearstrings()));
                 })
                 { IsBackground = true };
+                // End new Thread workload -------------------- //
                 crypt.Start();
             }
-            else label2.Text = "Incorrect Password!";
+            else
+            {
+                label2.Text = "Incorrect Password!";
+                label2.Refresh();
+                Thread.Sleep(1000);
+                UpdateLabels();
+            }
+            
         }
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void TextBox1_TextChanged(object sender, EventArgs e)
         {
-            Key = " " + textBox1.Text;
-            checkstatus();
+            if (textBox1.Text == "") Key = " ";
+            else Key = textBox1.Text;
+            Checkstatus();
+        }
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            DelFile = checkBox1.Checked;
         }
     }
 }
